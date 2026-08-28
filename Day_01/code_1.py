e9e27633c96c1cfd53cbc5e7dc9a8ce2000d98b6
Md.Shamim <md.shamim8t8@gmail.com>
2026-08-28 18:52:39 +0600

    first commit

---
+++ Day_01/code_1.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("hello world with python")
+print("hello world with","new day")
+print("hello world with \nnew day")
---
+++ Day_01/variables.py
@@ -0,0 +1,8 @@
+name="Shamim"
+age=30
+pi=3.1416
+
+print(name,age,pi)
+print("your name is:",name)
+print("your age is:",age)
+print("The value of pi:",pi)
