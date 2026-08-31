88a8be385aee199f1c38b2201b883b20ff069829
Md.Shamim <md.shamim8t8@gmail.com>
2026-08-31 12:35:54 +0600

    added list method

--- Day_04/list.py
+++ Day_04/list.py
@@ -9,3 +9,26 @@ print(F"updated list is {marks}")
 print(marks[2:5])
 #reverse slicing
 print(("reverse marks: "),marks[-4:-1])
+
+
+#append=add a new value in list
+marks.append(72)
+print(f"after added a new value {marks}")
+
+#insert=add a new value in list with indexing
+marks.insert(4,60)
+print(f"after added a new value with indexing {marks}")
+
+#reverse the list
+print(f"before reverse the list: {marks}")
+marks.reverse()
+print(f"after reverse the list: {marks}")
+
+#sorting in ascending order
+marks.sort()
+print(f"ascending order sorting: {marks}")
+
+
+#sorting in descending order
+marks.sort(reverse=True)
+print(f"descending order sorting: {marks}")
