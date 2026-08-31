9c75bc7580f47a358ec586893528bb7c15c7f168
Md.Shamim <md.shamim8t8@gmail.com>
2026-08-31 12:22:22 +0600

    list in pytohn

---
+++ Day_04/list.py
@@ -0,0 +1,11 @@
+marks=[89,85,93,100,75,79,69]
+
+print(marks)
+print(len(marks))
+#update value of list
+marks[3]=84
+print(F"updated list is {marks}")
+#slicing
+print(marks[2:5])
+#reverse slicing
+print(("reverse marks: "),marks[-4:-1])
