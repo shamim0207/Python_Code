b6cecbdb557db908d97682f58f7d1d72c927c7cc
Md.Shamim <md.shamim8t8@gmail.com>
2026-09-07 17:25:21 +0600

    practice problem solve

---
+++ practice_problem/problem21.py
@@ -0,0 +1,4 @@
+n=int(input("Enter your number(non negative): "))
+if(n>=0):
+    for i in range(n+1):
+        print(i**2)
---
+++ practice_problem/problem22.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your first number: "))
+b=int(input("Enter your second number: "))
+c=int(input("Enter your third number: "))
+d=int(input("Enter your fourth number: "))
+
+value=(a**b)+(c**d)
+print(value)
